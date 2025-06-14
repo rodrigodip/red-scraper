032c915c4b06348f7fe31a65b8b74f24f974186a
rodrigodip <rodrigodip@gmail.com>
2025-06-14 09:47:02 -0300

    Valida estrutura do conteúdo evitando as coleções listadas.

--- src/api/main.go
+++ src/api/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly/v2"
+	"regexp"
+	"strings"
 )
 
 type obra struct {
 	Titulo string
 	Ano    string
+	Mes    string
 	Link   string
 }
 type camarada struct {
@@ -28,9 +31,34 @@ func main() {
 		fmt.Println(e.Text)
 
 	})
-	c.OnHTML("tr td", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
-		//	fmt.Println( e.Text)
+	c.OnHTML("tbody", func(e *colly.HTMLElement) {
+		temp := obra{}
+
+		e.ForEach("tr", func(i int, e *colly.HTMLElement) {
+			// handles data da obra separando Ano e Mês
+			dataObra := e.ChildText("td:nth-of-type(1)")
+			ano, mes, temMes := strings.Cut(dataObra, " - ")
+			temp.Ano = ano
+
+			// Verifica validade do conteúdo
+			pattern := `^[0-9]{4}` // Apenas conteúdos que possuem a estrutura Ano - Titulo - Link são coletadas
+			isValido := regexp.MustCompile(pattern)
+
+			if isValido.MatchString(ano) {
+				if temMes {
+					temp.Mes = mes
+				} else {
+					temp.Mes = "Indefinido"
+				}
+				fmt.Println("Ano :", temp.Ano, "Mês :", temp.Mes)
+			}
+			//titulo := e.ChildText("td:nth-of-type(2)")
+			//link := e.ChildAttr("td", "a")
+
+		})
+
+		fmt.Println(temp.Titulo)
+		fmt.Println(temp.Link)
 	})
 
 	// Before making a request print "Visiting ..."
